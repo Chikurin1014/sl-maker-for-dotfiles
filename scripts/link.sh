ba46f25a5f64cc3da7b42f35c3199ec81593d98a
Chikurin1014 <chikurin1014@gmail.com>
2024-04-18 11:39:44 +0900

    add `mkdir -p`

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 for file in $ORIGIN_FILES; do
+    echo "Linking ${file#"$ORIGIN_ROOT/"}"
     destination_path=$(get_dest $file)
-    echo "$file"
-    echo "-> $destination_path"
+    echo "  $file"
+    echo "  -> $destination_path"
     if [[ $DRY_RUN = false ]]; then
         ln -s "$file" "$destination_path"
     fi
--- scripts/resolve.sh
+++ scripts/resolve.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
 for file in $ORIGIN_FILES; do
-    echo "Checking $file"
+    echo "Checking ${file#"$ORIGIN_ROOT/"}"
     destination_path=$(get_dest $file)
+
+    mkdir -p $(dirname $destination_path)
     status=$(check $destination_path)
 
     if [[ $status = $LINK_EXISTS ]]; then
         echo "  Unlink $destination_path"
-
         if [[ $DRY_RUN = false ]]; then
             unlink "$destination_path"
         fi
-
     elif [[ $status = $FILE_EXISTS ]]; then
         echo "  Move $destination_path to $destination_path.old (if $destination_path.old already exists, remove old one)"
-
         if [[ $DRY_RUN = false ]]; then
             [[ -e "$destination_path.old" ]] && rm "$destination_path.old"
             mv "$destination_path" "$destination_path.old"
